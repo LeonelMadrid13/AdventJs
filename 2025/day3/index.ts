dbacc90b77bf057bcea8b74e40d716f9cc2e7fea
LeonelMadrid13 <leoneleduardomadrid@hotmail.com>
2025-12-06 11:59:24 -0600

    feat: implement drawGift function to create a gift representation based on size and symbol

--- 2025/day3/index.ts
+++ 2025/day3/index.ts
@@ -1,4 +1,11 @@
 function drawGift(size: number, symbol: string): string {
   // Code here
-  return ''
+  if (size < 2 ) return '';
+  const top : string = symbol.repeat(size) + '\n'
+  let middle : string = '';
+  if (size > 2) {
+    middle = (symbol + ' '.repeat(size - 2) + symbol + '\n').repeat(size - 2)
+  }
+  const bottom : string = symbol.repeat(size)
+  return top + middle + bottom
 }
