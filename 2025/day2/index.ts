a50552fc3a91e0945d4043fda0827811688077ac
LeonelMadrid13 <leoneleduardomadrid@hotmail.com>
2025-12-06 11:48:48 -0600

    feat: implement manufactureGifts function to produce gifts based on input quantities

--- 2025/day2/index.ts
+++ 2025/day2/index.ts
@@ -1,4 +1,14 @@
 function manufactureGifts( giftsToProduce: Array<{ toy: string, quantity: number }>): string[] {
   // Code here
-  return []
+    let gifts = [];
+    for(let gift of giftsToProduce){
+        let toy = gift.toy
+        let quantity = gift.quantity
+        if (quantity > 0) {
+          for (let i = 0; i < quantity; i++) {
+            gifts.push(toy)
+          }
+        }
+    }
+  return gifts
 }
