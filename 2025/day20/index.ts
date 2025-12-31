98901a79412c2a465ccba00490dfa1685db7f4b2
LeonelMadrid13 <leoneleduardomadrid@hotmail.com>
2025-12-31 14:03:42 -0600

    feat: Implement dropGifts function with test cases for gift dropping logic

--- 2025/day20/index.ts
+++ 2025/day20/index.ts
@@ -1,5 +1,75 @@
 // Script for day20 
 function dropGifts(warehouse: string[][], drops: number[]): string[][] {
-  // Code here
-  return []
+  for (const drop of drops) {
+    for (let row = warehouse.length - 1; row >= 0; row--) {
+      if (warehouse[row][drop] === '.') {
+        warehouse[row][drop] = '#'
+        break
+      }
+    }
+  }
+  return warehouse
 }
+
+
+console.log(dropGifts(
+  [
+    ['.', '.', '.'],
+    ['.', '#', '.'],
+    ['#', '#', '.']
+  ],
+  [0]
+))
+/*
+[
+  ['.', '.', '.'],
+  ['#', '#', '.'],
+  ['#', '#', '.']
+]
+*/
+
+console.log(dropGifts(
+  [
+    ['.', '.', '.'],
+    ['#', '#', '.'],
+    ['#', '#', '#']
+  ],
+  [0, 2]
+))
+/*
+[
+  ['#', '.', '.'],
+  ['#', '#', '#'],
+  ['#', '#', '#']
+]
+*/
+
+console.log(dropGifts(
+  [
+    ['.', '.', '.'],
+    ['.', '.', '.'],
+    ['.', '.', '.']
+  ],
+  [0, 1, 2]
+))
+/*
+[
+  ['.', '.', '.'],
+  ['.', '.', '.'],
+  ['#', '#', '#']
+]
+*/
+
+console.log(dropGifts(
+  [
+    ['#', '#'],
+    ['#', '#']
+  ],
+  [0, 0]
+))
+/*
+[
+  ['#', '#']
+  ['#', '#']
+]
+*/
